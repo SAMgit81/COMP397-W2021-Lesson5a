985cc896b052ee011c3fc9f9b51236567f274615
Samuel <Samueljambo81@gmail.com>
2021-02-19 20:26:15 -0500

    added death plane to main scene

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    [Header("Controller Properties")]
     public CharacterController controller;
 
+    [Header("Movement")]
     public float maxSpeed = 10.0f;
     public float gravity = -30.0f;
     public float jumpHeight = 3.0f;
+    public Vector3 velocity;
 
+    [Header("Ground Detection Properties")]
     public Transform groundCheck;
     public float groundRadius = 0.5f;
     public LayerMask groundMask;
-
-    public Vector3 velocity;
     public bool isGrounded;
+
+    [Header("MinMap")]
+    public GameObject minMap;
+
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        minMap.SetActive(false);
     }
 
     // Update is called once per frame - once every 16.6666ms
@@ -49,6 +56,11 @@ public class PlayerBehaviour : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            minMap.SetActive(!minMap.activeInHierarchy);
+        }
     }
 
     void OnDrawGizmos()
